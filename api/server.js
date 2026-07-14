38172fc36535cd5389d31ddee9d8853dd5220c1f
Eluan <eluanambrosio3@gmail.com>
2026-07-14 19:47:24 -0300

    ohh

--- api/server.js
+++ api/server.js
@@ -3,21 +3,24 @@ const { databaseConnection } = require('./database/connection');
 databaseConnection();
 
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
 const PORT = process.env.PORT || 3031;
 
 const roomsRoute = require('./routes/roomsRoute');
 const clientsRoute = require('./routes/clientsRoute');
 const reservationRoute = require('./routes/reservationRoute');
-const authRoute=require("./routes/authRoute");
+const authRoute = require('./routes/authRoute');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/rooms', roomsRoute);
 app.use('/clients', clientsRoute);
 app.use('/reservations', reservationRoute);
-app.use("/auth",authRoute);
-
+app.use('/auth', authRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
